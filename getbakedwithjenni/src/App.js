3289995a41484408a3e3fba09ad18efff5026dd5
Ken Ingram <ken.ingram@gmail.com>
2021-02-23 13:50:29 -0800

    Preliminary scaffolding

--- getbakedwithjenni/src/App.js
+++ getbakedwithjenni/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Router } from 'react-router-dom'
+import Layout from './components/shared/Layout/Layout'
+
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path ="/" />
+        <Route exact path ="/admin" />
+        <Route exact path ="/login" />
+        <Route exact path ="/customer" />
+        <Route exact path ="/goods" />
+      </Switch>
     </div>
   );
 }
---
+++ getbakedwithjenni/src/components/shared/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = (props) => {
+
+  return (
+    <div className="footer">
+      Footer Text here
+    </div>
+  )
+}
+
+
+export default Footer
