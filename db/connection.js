97ea049737737e6ff215d301f3440948175eae6d
Ken Ingram <ken.ingram@gmail.com>
2021-03-16 08:56:50 -0700

    Testing database access.

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,37 @@
-const postgres = require('postgres')
-const sql = postgres(`postgres://${$DB_USER}:${DB_PW}@${DB_HOST}:${DB_PORT}/${DB}`)
+const path = require('path')
+require('dotenv').config({
+  path: path.resolve('../.env')
+})
 
+const postgres = require('postgres')
+// const db = postgres(`postgres://${process.env.DB_USER}:${process.env.DB_PW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`)
+const db = postgres({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB,
+  username: process.env.DB_USER,
+  pass: process.env.DB_PW
+})
+
+// const db = postgres({ ...options })
+
+
+const test = async () => {
+  let results
+  try {
+    results = await db`
+      select * from products
+    `
+  } catch (e){
+    console.log(e)
+  } finally {
+    db.end()
+  }  
+  
+  console.log(results)
+}
+
+test()
 module.exports = {
-  sql
+  db
 }
